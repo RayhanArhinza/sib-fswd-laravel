4cf9869c041fee5401426626fb0848a7a1e477e0
RayhanArhinza <126132862+RayhanArhinza@users.noreply.github.com>
2023-05-24 20:18:01 +0700

    Add files via upload

---
+++ Task 23/arkatama-laravel/storage/framework/views/152a18fa85b6035c46c0763b8eba9abaf5fcb6bd.php
@@ -0,0 +1,25 @@
+<?php $__env->startSection('content'); ?>
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="my-4">Role</h1>
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\arkatama-laravel\resources\views/role/index.blade.php ENDPATH**/ ?>
